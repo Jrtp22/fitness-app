9755a6758f307af04cb7cfd7b4ebb139e56b338c
Jose Torres <jrtp22@gmail.com>
2023-12-13 20:28:04 -0800

    added a controller and index.js

---
+++ server/index.js
@@ -0,0 +1,20 @@
+//modules
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//routes
+app.use('/fitness', require('./controllers/fitness'));
+
+//server
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
+})
