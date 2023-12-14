2af6338530010c58d998b55d6365d2b22e204ff7
Jose Torres <jrtp22@gmail.com>
2023-12-13 21:22:31 -0800

    finished writing the fit model now need to test

--- server/models/fit.js
+++ server/models/fit.js
@@ -1,14 +1,22 @@
 "use strict";
 const { Model } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
-    class Fit extends Model {
-    Fit.init(
-        {
-            id: {
-                type: DataTypes.INTEGER,
-                primaryKey: true,
-                autoIncrement: true,
-            },
-            name: {
-                type: DataTypes.STRING,
-}
+    const Fit = sequelize.define("Fit", {
+        fitnessId: {
+            type: DataTypes.SMALLINT,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        weekday: DataTypes.STRING,
+        muscle: DataTypes.STRING,
+        reps: DataTypes.INTEGER,
+        time: DataTypes.INTEGER,
+        checkBox: DataTypes.BOOLEAN,
+    }, {
+        underscored: true,
+        modelName: "Fit",
+    });
+};
+
+return Fit;
