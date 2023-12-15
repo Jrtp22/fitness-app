f3dfb1e34af75303efc9fcea361fb25501417ada
Bsssseep <131932227+Bsssseep@users.noreply.github.com>
2023-12-14 19:28:21 -0800

    Navbar

---
+++ client/src/components/navbar.js
@@ -0,0 +1,26 @@
+// components/Layout.jsx
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+function Layout({ children }) {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Fitness App</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#workouts">Workouts</Nav.Link>
+              <Nav.Link href="#profile">Profile</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container className="mt-3">{children}</Container>
+    </>
+  );
+}
+
+export default Layout;
