29641bc65bca8db03c76a4668028e6a8453fb702
Javier <Jay14539@gmail.com>
2023-12-18 18:45:36 -0800

    appjs update

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// App.js
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import React from 'react';
+import AuthForm from './AuthForm';
+
+function App() {
+  return (
+    <div className="App">
+      <AuthForm />
+    </div>
+  );
+}
+
+export default App;
+
