3ef51cfe212b639905bd427cffc0cb1263acda55
Bsssseep <131932227+Bsssseep@users.noreply.github.com>
2023-12-20 17:56:25 -0800

    Updated Card component

--- client/src/components/ProfileCard.js
+++ client/src/components/ProfileCard.js
@@ -1,47 +1,68 @@
-import React from 'react';
-import '../app.css';
+import React, { useState, useEffect } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
 
-class UserProfileCard extends React.Component {
-  render() {
-    const { name, age, height, goal } = this.props;
+function UserProfileCard() {
+  const { fitnessId } = useParams();
+  const history = useHistory();
+  const [fitness, setFitness] = useState(null);
 
-    return (
-      <div className="container mt-5">
-        <div className="row d-flex justify-content-center">
-          <div className="col-md-7">
-            <div className="card p-3 py-4">
-              <div className="text-center">
-                <img src="https://images.unsplash.com/photo-1527856263669-12c3a0af2aa6?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                 width="100" className="rounded-circle" alt="Profile" />
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/fitness/${fitnessId}`);
+      const resData = await response.json();
+      setFitness(resData);
+    };
+    fetchData();
+  }, [fitnessId]);
+
+  if (fitness === null) {
+    return <h1>Loading</h1>;
+  }
+
+  const deleteFitness = async () => {
+    await fetch(`${process.env.REACT_APP_SERVER_URL}/fitness/${fitness.fitnessId}`, {
+      method: 'DELETE',
+    });
+    history.push('/fitness');
+  };
+
+  return (
+    <div className="container mt-5">
+      <div className="row d-flex justify-content-center">
+        <div className="col-md-7">
+          <div className="card p-3 py-4">
+            <div className="text-center">
+              <img src={fitness.pic} alt={fitness.name} width="100" className="rounded-circle" />
+            </div>
+            <div className="text-center mt-3">
+              <h5 className="mt-2 mb-0">{fitness.name}</h5>
+              <span>{fitness.age}</span>
+
+              <div className="px-4 mt-1">
+                <p className="fonts">{fitness.height}</p>
               </div>
-              <div className="text-center mt-3">
-                <h5 className="mt-2 mb-0">{name}</h5>
-                <span>{age}</span>
+              <div className="px-4 mt-1">
+                <p className="fonts">{fitness.weight}</p>
+              </div>
+              <div className="px-4 mt-1">
+                <p className="fonts">{fitness.fitness_goal}</p>
+              </div>
+              <div className="px-4 mt-1">
+                <p className="fonts">{fitness.activity_level}</p>
+              </div>
 
-                <div className="px-4 mt-1">
-                  <p className="fonts">{height}</p>
-                </div>
-
-                <ul className="social-list">
-                  {socialLinks.map((link, index) => (
-                    <li key={goal}>
-                      <a href={link.url} target="_blank" rel="noopener noreferrer">
-                        <i className={`fa fa-${link.icon}`}></i>
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-
-                <div className="buttons">
-                  <button className="btn btn-outline-primary px-4">Work-Outs</button>
-                </div>
+              <div className="buttons">
+                <button className="btn btn-outline-primary px-4">Work-Outs</button>
+                <button className="btn btn-danger px-4" onClick={deleteFitness}>
+                  Delete
+                </button>
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default UserProfileCard;
